8e2716d80424c335fee5df36c24c46cbf6420ac6
Chris Ponticello <christopher.ponticello@uqconnect.edu.au>
2012-09-05 19:08:33 +1000

    finished print_packet(), added global definitions for packet command types

--- ftpclient/srtp_common.h
+++ ftpclient/srtp_common.h
@@ -12,6 +12,18 @@
 
 #include "list.h"
 
+/*
+ * Define the commands here
+ */
+#define CMD_NONE ((uint8_t)0)
+#define CMD_ACK ((uint8_t)1)
+//...
+#define MAX_CMD_VALUE ((uint8_t)0)
+
+const char* command_strings[(size_t)(MAX_CMD_VALUE+1)]={
+    "", "ACK"
+};
+
 #define PACKET_SIZE 512
 #define HEADER_SIZE 2
 #define PAYLOAD_SIZE (PACKET_SIZE - HEADER_SIZE)
@@ -20,9 +32,9 @@ enum PKTDIR {
   SEND, RECV
 };
 
-int debug = 0;
-
 #define d2(...) fprintf(stderr,__VA_ARGS__)
+
+int debug = 0;
 
 void _debug(const char *format, va_list args) {
     fprintf(stderr, "Debug: ");
@@ -38,33 +50,33 @@ void d(const char *format, ...) {
     }
 }
 
-void print_packet(struct sockaddr_in* addr_local, struct sockaddr_in* addr_remote, char* pktbuffer, enum PKTDIR dir) {
-    //uint16_t port_local=0;
-    //uint16_t port_remote=0;
-    //char str_local[16];
-    //char str_remote[16];
-    char cmdstr[16];
-    char timestr[16];
-    char arrow='<';
-    //uint8_t seq=0;
-    uint8_t len=0;
+char* command2str(uint8_t command) {
+    static char s[16];
 
-    //strcpy(str_local, "xxx.xxx.xxx"); 
-    //strcpy(str_remote, "xxx.xxx.xxx");
-    strcpy(cmdstr, "ACK");
-    strcpy(timestr, "hh:mm:ss");
+    if (command>MAX_CMD_VALUE) {
+        sprintf(s,"?%u?",command);
+        return (char*)s;
+    } else return (char*)command_strings[command];
+}
+
+void print_packet(struct sockaddr_in* addr_local, struct sockaddr_in* addr_remote, char* pktbuffer, enum PKTDIR dir) {
+    char timestr[32]={'\0'};
+    char arrow='<';
+    time_t rawtime;
+    struct tm * timeinfo;
 
     arrow = (dir==SEND?'<':'>');
-    //port_local = ntohs(addr_local->sin_port);
-    //port_remote = ntohs(addr_remote->sin_port);
-    //str_remote=inet_ntoa(addr_remote->sin_addr.s_addr);
-    //str_local=inet_ntoa(addr_local->sin_addr.s_addr);
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(timestr,32,"%H:%M:%S",timeinfo);
 
     d2("[%u] %s | %s:%u %c %s %c %s:%u | seq=%u len=%u\n", 
         getpid(), timestr, inet_ntoa(addr_remote->sin_addr), 
         ntohs(addr_remote->sin_port), arrow, 
-        cmdstr, arrow, inet_ntoa(addr_local->sin_addr), 
-        ntohs(addr_local->sin_port), pktbuffer[1], len);
+        command2str(pktbuffer[0]), arrow, inet_ntoa(addr_local->sin_addr), 
+        ntohs(addr_local->sin_port), pktbuffer[1], 
+        ntohs((uint16_t)pktbuffer[2]));
 }
 
 #endif
