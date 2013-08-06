0f8800dc46648710740692f0de6e8c99cafdcc78
Sam Macpherson <sam.mack91@gmail.com>
2013-08-06 17:55:22 +1000

    srtp: added #ifdefs to switch between TCP and SRTP

--- srtp/srtp.c
+++ srtp/srtp.c
@@ -1,37 +1,76 @@
 #include "srtp.h"
+#include "srtp_impl.h"
+
+#define USE_STD_TCP
 
 int srtp_socket( int domain, int type, int protocol ){
-  return socket( domain, type, protocol );
+  #ifdef USE_STD_TCP
+    return socket( domain, type, protocol );
+  #else
+    return _srtp_socket( domain, type, protocol );
+  #endif
 }
 
 int srtp_listen( int socket, int backlog ) {
-  return listen( socket, backlog );
+  #ifdef USE_STD_TCP
+    return listen( socket, backlog );
+  #else
+    return _srtp_listen( socket, backlog );
+  #endif
 }
 
 int srtp_bind( int socket, const struct sockaddr* address, socklen_t address_len ){
-  return bind( socket, address, address_len );
+  #ifdef USE_STD_TCP
+    return bind( socket, address, address_len );
+  #else
+    return _srtp_bind( socket, address, address_len );
+  #endif
 }
 
 int srtp_accept( int socket, struct sockaddr* address, socklen_t* address_len ){
-  return accept( socket, address, address_len );
+  #ifdef USE_STD_TCP
+    return accept( socket, address, address_len );
+  #else
+    return _srtp_accept( socket, address, address_len );
+  #endif
 }
 
 int srtp_connect( int socket, const struct sockaddr* address, socklen_t address_len ){
-  return connect( socket, address, address_len );
+  #ifdef USE_STD_TCP
+    return connect( socket, address, address_len );
+  #else
+    return _srtp_connect( socket, address, address_len );
+  #endif
 }
 
 int srtp_shutdown( int socket, int how ){
-  return shutdown( socket, how );
+  #ifdef USE_STD_TCP
+    return shutdown( socket, how );
+  #else
+    return _srtp_shutdown( socket, how );
+  #endif
 }
 
 int srtp_getpeername( int socket, struct sockaddr* address, socklen_t* address_len ){
-  return getpeername( socket, address, address_len );
+  #ifdef USE_STD_TCP
+    return getpeername( socket, address, address_len );
+  #else
+    return _srtp_getpeername( socket, address, address_len );
+  #endif
 }
 
 int srtp_send( int socket, const void* message, size_t length, int flags ){
-  return send( socket, message, length, flags );
+  #ifdef USE_STD_TCP
+    return send( socket, message, length, flags );
+  #else
+    return _srtp_send( socket, message, length, flags );
+  #endif
 }
 
 int srtp_recv( int socket, void* buffer, size_t length, int flags ){
-  return recv( socket, buffer, length, flags );
+  #ifdef USE_STD_TCP
+    return recv( socket, buffer, length, flags );
+  #else
+    return _srtp_recv( socket, buffer, length, flags );
+  #endif
 }
---
+++ srtp/srtp_impl.h
@@ -0,0 +1,27 @@
+#ifndef _SRTP_IMPL_H_
+#define _SRTP_IMPL_H_
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+extern int _srtp_socket( int domain, int type, int protocol );
+
+extern int _srtp_listen( int socket, int backlog );
+
+extern int _srtp_bind( int socket, const struct sockaddr* address, socklen_t address_len );
+
+extern int _srtp_accept( int socket, struct sockaddr* address, socklen_t* address_len );
+
+extern int _srtp_connect( int socket, const struct sockaddr* address, socklen_t address_len );
+
+extern int _srtp_shutdown( int socket, int how );
+
+extern int _srtp_getpeername( int socket, struct sockaddr* address, socklen_t* address_len );
+
+extern int _srtp_send( int socket, const void* message, size_t length, int flags );
+
+extern int _srtp_recv( int socket, void* buffer, size_t length, int flags );
+
+#endif // _SRTP_IMPL_H_
