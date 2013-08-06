c25c12fb1ab0be7ea9365d2fa4c85b8e98ce737e
Sam Macpherson <sam.mack91@gmail.com>
2013-08-06 23:48:58 +1000

    initial directory structure and srtp.h stub

---
+++ srtp/srtp.h
@@ -0,0 +1,27 @@
+#ifdef _SRTP_H_
+#define _SRTP_H_
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+extern int srtp_socket( int domain, int type, int protocol );
+
+extern int srtp_listen( int socket, int backlog );
+
+extern int srtp_bind( int socket, const struct sockaddr* address, socklen_t address_len );
+
+extern int srtp_accept( int socket, struct sockaddr* address, socklen_t* address_len );
+
+extern int srtp_connect( int socket, const struct sockaddr* address, socklen_t address_len );
+
+extern int srtp_shutdown( int socket, int how );
+
+extern int srtp_getpeername( int socket, struct sockaddr* address, socklen_t* address_len );
+
+extern int srtp_send( int socket, const void* message, size_t length, int flags );
+
+extern int srtp_recv( int socket, void* buffer, size_t length, int flags );
+
+#endif // _SRTP_H_
