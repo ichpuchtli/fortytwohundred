dd5b34d03107a44688abc2274599202c7e1b3b8f
Sam <sam.mack91@gmail.com>
2013-08-13 23:08:09 -0400

    srtp: added code comments describing the required internal functionality

--- srtp/srtp_impl.cpp
+++ srtp/srtp_impl.cpp
@@ -9,8 +9,13 @@
 
 // C++ Headers
 #include <map>
+#include <string>
 
-std::map<int, char*> fd2fifo;
+// fifo_fd -> fifo_path
+std::map<int, std::string> fd2fifo;
+
+// sha1 hash(addr) -> fifo_fd 
+std::map<std::string, int> id2fd;
 
 const int FIFO_PERMISSIONS = 0666;
 
@@ -38,25 +43,72 @@ int _srtp_socket( int domain, int type, int protocol ){
     return -1;
   }
   
-  fd2fifo.insert({fd, filename});
+  fd2fifo.insert({fd, std::string(filename)});
   
   return fd;
   
 }
 
-int _srtp_listen( int socket, int backlog ){return 0;}
+int _srtp_listen( int socket, int backlog ){
+  
+  // detach event loop thread
+  return 0;
+}
 
-int _srtp_bind( int socket, const struct sockaddr* address, socklen_t address_len ){return 0;}
+int _srtp_bind( int socket, const struct sockaddr* address, socklen_t address_len ){
+  
+  // id = hash(address)
+  // id2fd.insert(id, socket);
+  return 0;
+  
+}
 
-int _srtp_accept( int socket, struct sockaddr* address, socklen_t* address_len ){return 0;}
+int _srtp_accept( int socket, struct sockaddr* address, socklen_t* address_len ){
+  
+  // wait/block for unique client connection
+  // create fifo
+  // append fifo to list of active connections
+  // return fifo_fd
+  return 0;
 
-int _srtp_connect( int socket, const struct sockaddr* address, socklen_t address_len ){return 0;}
+}
 
-int _srtp_shutdown( int socket, int how ){return 0;}
+int _srtp_connect( int socket, const struct sockaddr* address, socklen_t address_len ){
+  
+  // request connection
+  // wait for ack
+  // detach event loop thread
+  // create fifo
+  // append fifo to list of active connections
+  // return fifo_fd
+  
+  return 0;
+
+}
+
+int _srtp_shutdown( int socket, int how ){
+  
+  // send shutdown packet
+  // stop event loop
+  // join event loop thread
+  // unlink fifo
+  
+  return 0;
+}
 
 int _srtp_getpeername( int socket, struct sockaddr* address, socklen_t* address_len ){return 0;}
 
-int _srtp_send( int socket, const void* message, size_t length, int flags ){return 0;}
+int _srtp_send( int socket, const void* message, size_t length, int flags ){
+  
+  // write to fifo  
+  // identical to write(fd, message, length)
+  
+  return 0;
+}
 
-int _srtp_recv( int socket, void* buffer, size_t length, int flags ){return 0;}
-
+int _srtp_recv( int socket, void* buffer, size_t length, int flags ){
+  
+  // read from fifo
+  // identical to read(fd, buffer, length)
+  return 0;
+}
